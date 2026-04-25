17e7c270f076d18ca4141ef1b3a657b75a6640ba
Neel Patel <neel85800@gmail.com>
2026-04-25 13:49:43 -0500

    Initial commit

---
+++ BMP_and_compass/ALl_sensors/ALl_sensors.ino
@@ -0,0 +1,204 @@
+#include <Wire.h>
+#include <LSM303.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include <MQ135.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+#define PIN_MQ135 A2
+#define OUTPUT_READABLE_ACCELGYRO
+
+MPU6050 mpu;
+LSM303 compass;
+Adafruit_BMP280 bmp;
+MQ135 mq135_sensor(PIN_MQ135);
+
+float temperature = 21.0; // Assume current temperature. Recommended to measure with DHT22
+float humidity = 25.0; // Assume current humidity. Recommended to measure with DHT22
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+bool blinkState;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  
+  /*
+  Calibration values; the default values of +/-32767 for each axis
+  lead to an assumed magnetometer bias of 0. Use the Calibrate example
+  program to determine appropriate values for your particular unit.
+  */
+  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+
+// BMP 280
+ Serial.begin(115200);
+  while ( !Serial ) delay(100);   // wait for native usb
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); 
+
+// MPU6050
+ #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin(); 
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  Serial.begin(115200); //Initializate Serial wo work well at 8MHz/16MHz
+
+  /*Initialize device and check connection*/ 
+  Serial.println("Initializing MPU...");
+  mpu.initialize();
+  Serial.println("Testing MPU6050 connection...");
+  if(mpu.testConnection() ==  false){
+    Serial.println("MPU6050 connection failed");
+    while(true);
+  }
+  else{
+    Serial.println("MPU6050 connection successful");
+  }
+
+  /* Use the code below to change accel/gyro offset values. Use MPU6050_Zero to obtain the recommended offsets */ 
+  Serial.println("Updating internal sensor offsets...\n");
+  mpu.setXAccelOffset(0); //Set your accelerometer offset for axis X
+  mpu.setYAccelOffset(0); //Set your accelerometer offset for axis Y
+  mpu.setZAccelOffset(0); //Set your accelerometer offset for axis Z
+  mpu.setXGyroOffset(0);  //Set your gyro offset for axis X
+  mpu.setYGyroOffset(0);  //Set your gyro offset for axis Y
+  mpu.setZGyroOffset(0);  //Set your gyro offset for axis Z
+  /*Print the defined offsets*/
+  Serial.print("\t");
+  Serial.print(mpu.getXAccelOffset());
+  Serial.print("\t");
+  Serial.print(mpu.getYAccelOffset()); 
+  Serial.print("\t");
+  Serial.print(mpu.getZAccelOffset());
+  Serial.print("\t");
+  Serial.print(mpu.getXGyroOffset()); 
+  Serial.print("\t");
+  Serial.print(mpu.getYGyroOffset());
+  Serial.print("\t");
+  Serial.print(mpu.getZGyroOffset());
+  Serial.print("\n");
+
+  /*Configure board LED pin for output*/ 
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  compass.read();
+  
+  /*
+  When given no arguments, the heading() function returns the angular
+  difference in the horizontal plane between a default vector and
+  north, in degrees.
+  
+  The default vector is chosen by the library to point along the
+  surface of the PCB, in the direction of the top of the text on the
+  silkscreen. This is the +X axis on the Pololu LSM303D carrier and
+  the -Y axis on the Pololu LSM303DLHC, LSM303DLM, and LSM303DLH
+  carriers.
+  
+  To use a different vector as a reference, use the version of heading()
+  that takes a vector argument; for example, use
+  
+    compass.heading((LSM303::vector<int>){0, 0, 1});
+  
+  to use the +Z axis as a reference.
+  */
+  float heading = compass.heading();
+  
+  Serial.println(heading);
+  delay(100);
+
+// BMP280
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
+
+// MQ135
+  float rzero = mq135_sensor.getRZero();
+  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
+  float resistance = mq135_sensor.getResistance();
+  float ppm = mq135_sensor.getPPM();
+  float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
+
+  Serial.print("MQ135 RZero: ");
+  Serial.print(rzero);
+  Serial.print("\t Corrected RZero: ");
+  Serial.print(correctedRZero);
+  Serial.print("\t Resistance: ");
+  Serial.print(resistance);
+  Serial.print("\t PPM: ");
+  Serial.print(ppm);
+  Serial.print("\t Corrected PPM: ");
+  Serial.print(correctedPPM);
+  Serial.println("ppm");
+
+  delay(300);
+
+// MPU6050
+ /* Read raw accel/gyro data from the module. Other methods commented*/
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  //mpu.getAcceleration(&ax, &ay, &az);
+  //mpu.getRotation(&gx, &gy, &gz);
+
+  /*Print the obtained data on the defined format*/
+  #ifdef OUTPUT_READABLE_ACCELGYRO
+    Serial.print("a/g:\t");
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.println(gz);
+  #endif
+
+  #ifdef OUTPUT_BINARY_ACCELGYRO
+    Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+    Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+    Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+    Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+    Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+    Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+  #endif
+
+  /*Blink LED to indicate activity*/
+  blinkState = !blinkState;
+  digitalWrite(LED_BUILTIN, blinkState);
+}
